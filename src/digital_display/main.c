3923c5a6420e31093dad2128d827be54d7ac2f13
DrPepperMD <51042590+DrPepperMD@users.noreply.github.com>
2019-11-24 02:48:15 +1100

    Added a basic digital-like display

---
+++ src/digital_display/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define example_int "00:00"
+
+int main()
+{
+	bool loop_stop = false;
+	// to be honest I could not be doinked about having to make a thing that compares char's...
+	// over enginering is my middle name!
+	// also premium spaghetti
+loop:
+	for (int i = 0; i < 25; i++)
+	{
+		printf("-");
+	}
+	if (loop_stop == true)
+	{
+		goto end;
+	}
+	else
+	{
+		printf("\n| %s | %s | %s |\n", example_int, example_int, example_int);
+		loop_stop = true;
+		goto loop;
+	}
+end:
+	return 0;
+}
